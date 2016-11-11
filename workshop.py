8b46d40ce57ad57b4591284956eec09a73c8fe40
psuAI <psu.ai.club@gmail.com>
2016-11-11 13:22:58 -0500

    Update workshop.py

--- workshop.py
+++ workshop.py
@@ -29,7 +29,7 @@ import os.path
 unknown = "<UNK>"
 trainingDataSpam = "data/spam"
 trainingDataHam = "data/ham"
-
+smothing = 1e-5
 
 def load_tokens(email_path):
     """DO NOT EDIT THIS FUNCTION! Returns a list of all words in a file."""
@@ -50,9 +50,14 @@ def get_contents(mail_path):
 
     return contents
 
+# the calculation of the log of possibility, put zero for value if it is unknown
+def log_possibility(value, total, counter):
+    return math.log(float(value + smoothing) / (total + smoothing * (len(counter) + 1)))
+
+
 class SpamFilter(object):
 
-    def __init__(self, smoothing):
+    def __init__(self):
         # all the class variables are declared right here
 
         # step 1: count through the file
@@ -70,6 +75,9 @@ class SpamFilter(object):
         spam_contents = get_contents(trainingDataSpam)  # List of all the file contents from spam folder
         ham_contents = get_contents(trainingDataHam)  # List of all the file contents from ham folder
 
+        # There are some examples of how to use log_possibility function, uncomment and change 'A word' to try it out
+        # self.spam_log['A word'] = log_possibility(self.spam_counter['A word'], self.total_spam, self.spam_counter)
+        
     # compare the possibility of spam and non-spam to get a True/False result
     def is_spam(self, email_path):
         pass
